f3d24d7ac5a170be7f0e1881ad783ce0945f7a64
xiao-hao-hao <1783692558@qq.com>
2020-02-25 11:36:24 +0800

    add redirection

--- shell/my_shell.cc
+++ shell/my_shell.cc
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #define MAX 1024
 using namespace std;
 
@@ -19,7 +21,8 @@ int main()
         pid_t id = fork();
         if (id == 0)
         {
-            //i = 0;
+            int fd, redirect_type = -1;
+            char *file;
             cout << "[zby@centos shell]#";
             gets(cmd);
             myargv[i] = cmd;
@@ -29,10 +32,50 @@ int main()
                 {
                     *p = '\0';
                     ++p;
-                    myargv[++i] = p;
+                    if (*p != '>')
+                        myargv[++i] = p;
                     continue;
                 }
+                if (*p == '>')
+                {
+                    ++redirect_type;
+                    *p = '\0';
+                    ++p;
+                }
+                if (*p == '>')
+                {
+                    ++redirect_type;
+                    *p = '\0';
+                    ++p;
+                }
+                if (redirect_type > -1)
+                {
+                    while (1)
+                    {
+                        if (isspace(*p))
+                        {
+                            *p = '\0';
+                            ++p;
+                        }
+                        else
+                        {
+                            file = p;
+                            break;
+                        }
+                    }
+                    break;
+                }
                 ++p;
+            }
+            if (redirect_type == 0)
+            {
+                fd = open(file, O_CREAT|O_WRONLY, 0664);
+                dup2(fd, 1);
+            }
+            else if (redirect_type == 1)
+            {
+                fd = open(file, O_CREAT|O_WRONLY|O_APPEND, 0664);
+                dup2(fd, 1);
             }
             myargv[++i] = NULL;
             execvp(myargv[0], myargv);
