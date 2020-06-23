2e6166175706dc0f06ec302ec911b816f5005554
xiao-hao-hao <1783692558@qq.com>
2020-06-23 23:36:42 +0800

    add selectServer

--- selectServer/selectServer.hpp
+++ selectServer/selectServer.hpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <unistd.h>
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <strings.h>
 #include <arpa/inet.h>
 
+#define MAX sizeof(fd_set)*8
 using namespace std;
-
-#define MAX sizeof(fd_set)*8;
 
 class selectServer
 {
@@ -46,6 +46,75 @@ public:
         int opt = 1;
         setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
     }
+
+    void handlerEvents(fd_set *rfds)
+    {
+        //循环处理对应就绪的事件
+        for (int i = 0; i < MAX; ++i)
+        {
+            if (vfds[i] < 0)
+                continue;
+            if (FD_ISSET(vfds[i], rfds))
+            {
+                if (vfds[i] == listen_sock)
+                {
+                    //1. 读取链接
+                    struct sockaddr_in peer;
+                    socklen_t len = sizeof(peer);
+                    int sock = accept(listen_sock, (struct sockaddr*)&peer, &len);
+                    if (sock < 0)
+                    {
+                        cerr << "accept error" << endl;
+                        continue;
+                    }
+                    cout << "get a new link..." << sock << endl;
+                    int j = 0;
+                    for (; j < MAX; ++j)
+                    {
+                        if (vfds[j] == -1)
+                            break;
+                    }
+                    if (j == MAX)
+                    {
+                        cout << "server full" << endl;
+                        close(sock);
+                    }
+                    else
+                    {
+                        //动态添加新的sock到全局数组当中，以便下次让select帮我们进行就绪时间检测
+                        vfds[j] = sock;
+                    }
+                }
+                else
+                {
+                    //2. 读取数据
+                    char buffer[10240];
+                    ssize_t s = recv(vfds[i], buffer, sizeof(buffer)-1, 0);
+                    if (s > 0)
+                    {
+                        buffer[s] = 0;
+                        cout << "client# " << buffer << endl;
+                    }
+                    else
+                    {//s < 0 or s == 0
+                        if (s == 0)
+                            cout << "peer link close, server close too!" << endl;
+                        else
+                            cout << "recv error, server close!" << endl;
+                        close(vfds[i]);
+                        //想办法移除老的sock，以防select出现问题
+                        vfds[i] = -1;
+                    }
+                }
+            }
+            else
+            {
+                //说明当前文件描述符上面的读事件是没有就绪的
+                cout << vfds[i] << " : curr fd is not ready!" << endl;
+            }
+        }
+    }
+
     void start()
     {
         vfds[0] = listen_sock;
